6199984d5a4583eccdc014ac6f74e45a38ad11dc
Bruno Campos Neves <bcneves13@gmail.com>
2022-09-20 16:28:10 -0300

    create destination folders if doesnt exists

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
 from actions.rating_value import rating_value
 def main():
     rating_value('News', 'rating_count_tot', 'news', 1)
     rating_value(['Music','Book'], 'rating_count_tot', 'music_book', 10)
 
 if __name__ == "__main__":
+    if not os.path.exists('csvs'):
+        os.makedirs('csvs')
+    if not os.path.exists('jsons'):
+        os.makedirs('jsons')
     main()
